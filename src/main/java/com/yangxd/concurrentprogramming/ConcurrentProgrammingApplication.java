f6df3d8760994f6b4bcee73bc3a6f2a8b09743f4
杨贤达 <xdyangchn@163.com>
2018-11-25 22:26:15 +0800

    Initial commit

---
+++ src/main/java/com/yangxd/concurrentprogramming/ConcurrentProgrammingApplication.java
@@ -0,0 +1,12 @@
+package com.yangxd.concurrentprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentProgrammingApplication.class, args);
+    }
+}
